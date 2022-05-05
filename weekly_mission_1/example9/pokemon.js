4c191684c91676af3497ee53f8c7222d9907f4ed
FatimaGloria <janettgloria1@gmail.com>
2022-05-05 00:25:14 -0500

    Se agrega codigo al archivo pokemon.js

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -3,3 +3,12 @@ class pokemon {
       this.name = name
     }
 }
+function sayHello(){
+  console.log('Mi pokemon ${this.name} te saluda')
+}
+
+function sayMessage(){
+  console.log('Mi pokemon ${this.name} dice:')
+}
+
+module.exports= pokemon
