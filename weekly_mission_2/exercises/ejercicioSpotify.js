091303a08e8af8616a793b63d55200de56bf3090
FatimaGloria <janettgloria1@gmail.com>
2022-05-06 21:25:33 -0500

    Se crea archivo ejercicioSpotify y se modulan objetos

---
+++ weekly_mission_2/exercises/ejercicioSpotify.js
@@ -0,0 +1,31 @@
+//Modular información de una app: Spotify
+
+//Objeto user 
+const user= {
+    userName: "Fatima",
+    followers: 10,
+    following: 13,
+    playlists: "All the playlists the user created"
+}
+
+//Objeto playlist
+const playlist= {
+    name: "Name",
+    playlistAuthor: "Creator Name",
+    playbackTime: "45 min",
+    songsList: "List of songs"
+}
+
+// Objeto Your Library
+const yourLibrary= {
+    playlists: {
+        yourFavorites: "All the songs you liked",
+        playlistsNameList: "List of all the playlists"
+    },
+    artists: "List of the favorites artists",
+    albums: "List of the favorites albums",
+    podcasts: {
+        newEpisodies: "New episodies collection",
+        podcastsNameList: "List of your favorite podcasts"
+    }
+}
