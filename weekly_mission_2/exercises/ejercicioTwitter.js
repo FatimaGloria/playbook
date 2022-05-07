46a01a96ca93dba01d7cd2fa45b861fb13183065
FatimaGloria <janettgloria1@gmail.com>
2022-05-06 19:53:50 -0500

    Se agrega segundo objeto de Twitter- Trending Topic

--- weekly_mission_2/exercises/ejercicioTwitter.js
+++ weekly_mission_2/exercises/ejercicioTwitter.js
@@ -1,3 +1,6 @@
+// Modulando información de una app: TWITTER 
+
+// Objeto user 
 const user = {
     name: "Fatima",
     username: "jaglo_095",
@@ -6,4 +9,12 @@ const user = {
     followers: 130,
     following: 200,
     tweets: 3450,
+}
+
+//Objeto trending topic
+const trending_topic ={
+    trendingNumber: 1,
+    topic: "Topic",
+    trendingName: "Name or Hashtag",
+    totalTweets: 60500
 }
