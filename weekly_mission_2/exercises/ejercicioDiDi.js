be4a9d05424e486989516a913589742ca3f080bc
FatimaGloria <janettgloria1@gmail.com>
2022-05-06 20:13:42 -0500

    Se crea archivo ejercicioDiDi y se modula el primer objeto: user

---
+++ weekly_mission_2/exercises/ejercicioDiDi.js
@@ -0,0 +1,10 @@
+//Modular información de una app: DiDi Food
+
+//Objeto user 
+const user= {
+    name: "Fatima",
+    lastName: "Gloria",
+    phoneNumber: "123 451 5412",
+    password: "pas5word",
+    email: "fatima.gloria@gmail.com"
+}
